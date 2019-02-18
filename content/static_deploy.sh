84084fc5531b914d19996c3e91599093d6a9eac4
Ansil H <ansilh@gmail.com>
2019-02-18 22:17:10 +0530

    content generator script

---
+++ content/static_deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# This file will copy contents from a source to destination
+# Use this file wisely
+
+# Video 1 - Karbon # This is a test file to see if we can reuse this script to copy big files 
+cp /roo/videos/Karbon.mp4 k8s-training-doc/content/18-Karbon/
